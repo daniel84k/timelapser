803476085da90d544565365849aba1ffb70d7958
daniel84k <daniel84k@Daniels-MacBook-Air.local>
2025-05-28 23:42:39 +0200

    aktualizacja

--- settings.py
+++
@@ -1,8 +0,0 @@
-from shutter_utils import set_closest_shutter_speed
-from iso_utils import set_closest_iso
-
-def set_camera_settings(shutter, iso, focal_length, rule):
-    max_shutter = rule / focal_length
-    set_closest_shutter_speed(shutter, max_shutter)
-    set_closest_iso(iso, max_iso=1600)
-
